577e906a5feaa4c6173b83f505036f8c3d3e4969
onest <ponest.casual@gmail.com>
2016-10-17 15:56:21 +0300

    first commit

---
+++ application/views/homepage.php
@@ -0,0 +1,12 @@
+<!DOCYTPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+</head>
+
+<body>
+<div id="sidebar"></div>
+<div id="sidebar_image"></div>
+</body>
+
+</html>
